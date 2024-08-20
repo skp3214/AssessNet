32bfe89443ccc7e4743badbec8122aae502a66b5
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2024-08-20 22:26:48 +0530

    fixing cors issue using without env

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,5 +1,5 @@
 const config={
-    apiUrl:String(import.meta.env.VITE_APIURL),
+    apiUrl:String("https://assessnet-cipherschools.onrender.com/api/auth"),
     emailJSServiceID:String(import.meta.env.VITE_APP_EMAILJS_SERVICE_ID),
     emailJSTemplateID:String(import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID),
     emailJSPublicKEY:String(import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY)
